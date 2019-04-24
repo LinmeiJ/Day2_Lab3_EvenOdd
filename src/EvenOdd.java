0c937bff5547ff125f26a54f96b1249fb1612f50
LinmeiJ <linmeijiang@hotmail.com>
2019-04-24 09:28:03 -0400

    second commit

--- src/EvenOdd.java
+++ src/EvenOdd.java
@@ -4,35 +4,37 @@ public class EvenOdd {
 
 	public static void main(String[] args) {
 		Scanner sc = new Scanner(System.in);
+		String name;
+		System.out.println("Please enter your name: ");
+		name = sc.nextLine();
+		
 		char userInput = 'y';
 		
 		do {
-			System.out.println("Please enter a number between 1 to 100: ");
+			System.out.println("Hi " + name + ", please enter a number between 1 to 100: ");
 			int num = sc.nextInt();
+			
 			while(num > 100 || num < 1) {
-				System.out.println("You entry is out of range, please try again: ");
+				System.out.println("sorry " + name + ", you entry is out of range, please try again: ");
 				num = sc.nextInt();
 			}
 			//System.out.println("test");
-			
 		
-		
-
 			if(num % 2 != 0) {
-				System.out.println(num + " is odd number");
+				System.out.println(num + " is an odd number.");
 				if(num > 60) {
-					System.out.println("Odd and over 60");
+					System.out.println("An odd number and over 60.");
 				}
 			}
 			else {
 				if(num >=2 && num < 25) {
-					System.out.println(num + " is even number and less than 25");
+					System.out.println(num + " is an even number and less than 25.");
 				}
 				else if(num >=26 && num <=60){
-					System.out.println(num + " is even number");
+					System.out.println(num + " is an even number.");
 				}
 				else {
-					System.out.println(num + " is even number and over 60");
+					System.out.println(num + " is an even number and over 60.");
 				}
 			}
 			System.out.println("Would you like to continue(y/n): ");
@@ -40,7 +42,7 @@ public class EvenOdd {
 			userInput = Character.toLowerCase(userInput);
 			
 		}while(userInput == 'y');
-		System.out.println("Goodbye");
+		System.out.println("Goodbye " + name + ".");
 		sc.close();
 	}
 
